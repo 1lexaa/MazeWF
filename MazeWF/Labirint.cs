f883c51f4b63796e02c70a7c26a97f170d5b897a
1lexaa <nice140@icloud.com>
2022-09-07 16:58:52 +0300

    Add project files.

---
+++ MazeWF/Labirint.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeWF
+{
+    internal class Labirint
+    {
+        public int height; 
+        public int width; 
+
+        public int amountOfMedals;
+
+        public MyObjects[,] maze;
+        public PictureBox[,] images;
+
+        public static Random r = new Random();
+        public Form parent;
+        public Labirint(Form parent, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.parent = parent;
+
+            maze = new MyObjects[height, width];
+            images = new PictureBox[height, width];
+
+            amountOfMedals = 0;
+
+            Generate();
+        }
+        private void Generate()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    MyObjects.MazeObjectType current = MyObjects.MazeObjectType.HALL;
+
+                    
+                    if (r.Next(5) == 0)
+                    {
+                        current = MyObjects.MazeObjectType.WALL;
+                    }
+
+                   
+                    if (r.Next(250) == 0)
+                    {
+                        amountOfMedals++;
+                        current = MyObjects.MazeObjectType.MEDAL;
+                    }
+                    if (r.Next(250) == 0)
+                    {
+                        current = MyObjects.MazeObjectType.HEAL;
+                    }
+                    
+                    if (r.Next(250) == 0)
+                    {
+                        current = MyObjects.MazeObjectType.ENEMY;
+                    }
+
+                   
+                    if (y == 0 || x == 0 || y == height - 1 | x == width - 1)
+                    {
+                        current = MyObjects.MazeObjectType.WALL;
+                    }
+
+                    
+                    if (x == 1 && y == 2)
+                    {
+                        current = MyObjects.MazeObjectType.CHAR;
+                    }
+
+                    if (x == 2 && y == 2 || x == width - 1 && y == height - 3)
+                    {
+                        current = MyObjects.MazeObjectType.HALL;
+                    }
+
+                    maze[y, x] = new MyObjects(current);
+                    images[y, x] = new PictureBox();
+                    images[y, x].Location = new Point(x * maze[y, x].width, y * maze[y, x].height);
+                    images[y, x].Parent = parent;
+                    images[y, x].Width = maze[y, x].width;
+                    images[y, x].Height = maze[y, x].height;
+                    images[y, x].BackgroundImage = maze[y, x].texture;
+                    images[y, x].Visible = false;
+                }
+            }
+        }
+        public void Show()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    images[y, x].Visible = true;
+                }
+            }
+        }
+    }
+}
---
+++ MazeWF/MyObjects.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeWF
+{
+    internal class MyObjects
+    {
+        public enum MazeObjectType { HALL, WALL, MEDAL, ENEMY, CHAR, HEAL };
+
+        public Bitmap[] images = {new Bitmap(@"C:\Users\1lexaa\source\repos\MazeWF\MazeWF\bin\Debug\net6.0-windows\Resources\hall.png"),
+            new Bitmap(@"C:\Users\1lexaa\source\repos\MazeWF\MazeWF\bin\Debug\net6.0-windows\Resources\wall.png"),
+            new Bitmap(@"C:\Users\1lexaa\source\repos\MazeWF\MazeWF\bin\Debug\net6.0-windows\Resources\medal.png"),
+            new Bitmap(@"C:\Users\1lexaa\source\repos\MazeWF\MazeWF\bin\Debug\net6.0-windows\Resources\enemy.png"),
+            new Bitmap(@"C:\Users\1lexaa\source\repos\MazeWF\MazeWF\bin\Debug\net6.0-windows\Resources\player.png"),
+            new Bitmap(@"C:\Users\1lexaa\source\repos\MazeWF\MazeWF\bin\Debug\net6.0-windows\Resources\aidkit.png")};
+
+        public MazeObjectType type;
+        public int width;
+        public int height;
+        public Image texture;
+
+        public MyObjects(MazeObjectType type)
+        {
+            this.type = type;
+            width = 16;
+            height = 16;
+            texture = images[(int)type];
+        }
+    }
+}
